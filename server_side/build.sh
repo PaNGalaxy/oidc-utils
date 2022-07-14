5a5ca32860b7ebfd904e65120214b28aebf54fed
Yakubov, Sergey <yakubovs@ornl.gov>
2022-07-14 17:18:22 -0400

    build rpm for opensuse

--- server_side/build.sh
+++ server_side/build.sh
@@ -1,2 +1,2 @@
-docker build --target no2fa -t ubuntu_sshd .
-docker build --target with2fa -t ubuntu_sshd_2fa .
+docker build --target no2fa -t ubuntu_sshd -f dockerfiles/Dockerfile .
+docker build --target with2fa -t ubuntu_sshd_2fa -f dockerfiles/Dockerfile  .
--- server_side/c/build.sh
+++ server_side/c/build.sh
@@ -3,4 +3,4 @@ mkdir build
 cd build
 cmake ..
 make
-make package
+cpack -G $1
