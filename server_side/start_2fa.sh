f9805fc7c4674d53748c741c6c5e1f5debb676ea
Yakubov, Sergey <yakubovs@ornl.gov>
2022-07-15 09:18:34 -0400

    use opensuse for 2fa image

--- server_side/start_2fa.sh
+++ server_side/start_2fa.sh
@@ -6,7 +6,7 @@ chmod 700 /home/test/auth
 chown test: -R /home/test
 
 if [ ! -e "/home/test/auth/.google_authenticator" ]; then
-  su test bash -c "google-authenticator -t -d -f -W -u -C -s /home/test/auth/.google_authenticator" > /home/test/auth/auth_secrets
+  su test bash -c "google-authenticator -t -d -f -W -u -s /home/test/auth/.google_authenticator" > /home/test/auth/auth_secrets
 fi
 
 cd /tmp/oidc
