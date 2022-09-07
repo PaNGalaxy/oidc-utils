5b6d5aeb710c9a8e0b91c23a610ea2d21d8599c2
Gregory Cage <cagege@ornl.gov>
2022-09-07 10:17:15 -0400

    Refactor oidc_pam to verif jwt using jwks by default. rename oidc-pam

--- server_side/python/oidc_pam.py
+++ server_side/python/oidc_pam.py
@@ -5,6 +5,7 @@
 '''
 PAM module for authenticating users via a OIDC token
 '''
+import base64
 import json
 import jwt
 import os
@@ -15,7 +16,7 @@ import logging
 from cryptography.hazmat.backends import default_backend
 from cryptography.x509 import load_der_x509_certificate
 
-logging.basicConfig(filename='/tmp/oidc.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='oidc.log', encoding='utf-8', level=logging.DEBUG)
 
 
 def logit(data):
@@ -26,7 +27,6 @@ def pam_sm_setcred(pamh, _flags, _argv):
     '''
     Default
     '''
-    logit("setcred")
     return pamh.PAM_SUCCESS
 
 
@@ -34,7 +34,6 @@ def pam_sm_acct_mgmt(pamh, _flags, _argv):
     '''
     Default
     '''
-    logit("acct mgmt")
     return pamh.PAM_SUCCESS
 
 
@@ -42,7 +41,6 @@ def pam_sm_open_session(pamh, _flags, _argv):
     '''
     Default
     '''
-    logit("open session")
     return pamh.PAM_SUCCESS
 
 
@@ -50,7 +48,6 @@ def pam_sm_close_session(pamh, _flags, _argv):
     '''
     Default
     '''
-    logit("close session")
     return pamh.PAM_SUCCESS
 
 
@@ -58,15 +55,13 @@ def pam_sm_chauthtok(pamh, _flags, _argv):
     '''
     Default
     '''
-    logit("chauthtok")
     return pamh.PAM_SUCCESS
 
 
 def pam_sm_authenticate(pamh, _flags, _argv):
     '''
     Authenticates a user via an OIDC token
-    '''    
-    logit("trying")
+    '''   
     
     # build access token
 
@@ -97,39 +92,35 @@ def pam_sm_authenticate(pamh, _flags, _argv):
     except pamh.exception as error:
         return error.pam_result
 
-    if (os.environ['PAM_OIDC_VERFIFICATION_TYPE'] == "jwks_url"):
-        return verify_token_jwt(pamh, user, access_token)
-    else:    
-        return verify_token_introspection(pamh, user, access_token)
+    return verify_token_jwt(pamh, user, access_token)
+       
 
-def verify_token_jwt(pamh, user, access_token):
+def verify_token_jwt(pamh, user, access_token, jwt_options):
     config = load_config_jwt(pamh)
     try:
         # Obtain appropriate cert from JWK URI
-        jwks_url = config['jwks_uri']
+        jwks_url = config['jwks_url']
+        
         key_set = requests.get(jwks_url, timeout=5)
 
         encoded_header, rest = access_token.split('.', 1)
-        headerobj = json.loads(base64.b64_decode(encoded_header).decode('utf8'))
-
+        headerobj = json.loads(base64.b64decode(encoded_header+ '==').decode('utf8'))
         key_id = headerobj['kid']
         for key in key_set.json()['keys']:
             if key['kid'] == key_id:
                 x5c = key['x5c'][0]
-                break
+            break
         else:
             raise jwt.DecodeError(f'Cannot find kid={kid}')
 
         cert = load_der_x509_certificate(base64.b64decode(x5c), default_backend())
-
         # Decode token (exp date is checked automatically)
         decoded_token = jwt.decode(
                 access_token,
-                key=certificate.public_key(),
+                key=cert.public_key(),
                 algorithms=['RS256'],
-                audience=self.setting('KEY')
+                options=jwt_options
             )
-
         # Check if correct user
         if decoded_token['preferred_username'] != user:
             logit('SSH user does not match token user: %s (ssh) !=v %s (token)' % (user,
@@ -148,37 +139,6 @@ def verify_token_jwt(pamh, user, access_token):
     logit('Login successful for user %s, token %s' % (user, access_token))
     return pamh.PAM_SUCCESS
 
-def verify_token_introspection(pamh, user, access_token):
-    logit('Attempting token verification through instrosepction URL.')
-    config = load_config_introspection(pamh)
-    try:
-        url = config['introspection_url']
-        logit(access_token)
-        data = {'token': access_token.strip(), 'client_id': config['client_id'],
-                'client_secret': config['client_secret']}
-        response = requests.post(url, data=data, timeout=5)
-        if response.status_code != requests.status_codes.codes.ok:
-            logit('Error checking introspecting token, server returned %d %s' % response.status_code, response.text)
-            return pamh.PAM_AUTH_ERR
-        token_info = response.json()
-        if 'active' not in token_info or token_info['active'] != True:
-            logit('Error checking introspecting token, token %s invalid, server response: %s' % (
-                access_token, response.text))
-            return pamh.PAM_AUTH_ERR
-        if 'preferred_username' not in token_info or token_info['preferred_username'] != user:
-            logit('wrong user name in token: %s, expected %s' % (access_token, user))
-            return pamh.PAM_AUTH_ERR
-        if config['check_2fa']:
-            if 'session_attribute' not in token_info or token_info['session_attribute'] != '2fa':
-                logit('missing 2fa in token: %s ' % access_token)
-                return pamh.PAM_AUTH_ERR
-    except Exception as error:
-        logit('Error introspecting token %s, error: %s' % (access_token, error))
-        return pamh.PAM_AUTH_ERR
-
-    logit('Login successful for user %s, token %s' % (user, access_token))
-    return pamh.PAM_SUCCESS
-
 def load_config(pamh):
     # Load config file 
     config_dpath = os.path.dirname(os.path.realpath(__file__))
@@ -194,13 +154,5 @@ def load_config_jwt(pamh):
         config = load_config(pamh)
         return next((config_item for config_item in config if config_item['verification_type'] == "jwks_url"))
     except Exception as error:
-        logit('Error loading configuration for jwt verification: %s' % error)\
-        return pamh.PAM_AUTH_ERR
-
-def load_config_introspection(pamh):
-    try:
-        config = load_config(pamh)
-        return next((config_item for config_item in config if config_item['verification_type'] == "introspection_url"))
-    except Exception as error:
-        logit('Error loading configuration for introspection verification: %s' % error)\
+        logit('Error loading configuration for jwt verification: %s' % error)
         return pamh.PAM_AUTH_ERR
